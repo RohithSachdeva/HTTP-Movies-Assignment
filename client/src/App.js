c1c2b08707affdd898312a6f4ce0cbaeb37d5214
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-05-21 13:24:13 -0700

    attempting to restructure movie to class component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
+import UpdateMovie from './Movies/UpdateMovie'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -15,7 +16,7 @@ const App = () => {
       .then(res => setMovieList(res.data))
       .catch(err => console.log(err.response));
   };
-
+  
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
